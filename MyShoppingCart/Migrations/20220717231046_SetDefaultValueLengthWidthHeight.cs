f1bf9ea92e89e6cb1e9dec18d905c08f4c747eec
David Warwick <bgmsd@live.com>
2022-07-17 16:32:47 -0700

    fixed migrations

---
+++ MyShoppingCart/Migrations/20220717231046_SetDefaultValueLengthWidthHeight.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyShoppingCart.Migrations
+{
+    public partial class SetDefaultValueLengthWidthHeight : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ MyShoppingCart/Migrations/20220717232529_SetDefaultValueLengthWidthHeight2.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyShoppingCart.Migrations
+{
+    public partial class SetDefaultValueLengthWidthHeight2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "height",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "length",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "width",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "height",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "length",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "width",
+                table: "Products");
+        }
+    }
+}
